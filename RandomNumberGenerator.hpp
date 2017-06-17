a21360a86acdd4098a77bf7b3ba39400275d97eb
Fergus Cooper <cooper@maths.ox.ac.uk>
2017-06-17 14:08:58 +0100

    Added normal distribution

--- RandomNumberGenerator.hpp
+++ RandomNumberGenerator.hpp
@@ -8,15 +8,24 @@ private:
     /** The main random number generator. **/
     std::mt19937 mMersenneTwister;
 
+    /** The normal distribution */
+    std::normal_distribution<double> mGenerateStandardNormal;
+
 public:
 
     explicit RandomNumberGenerator(unsigned seed)
-            : mMersenneTwister(seed)
+            : mMersenneTwister(seed),
+              mGenerateStandardNormal(0.0, 1.0)
     {
     }
 
     unsigned long GetRandomNumberFromMT()
     {
         return mMersenneTwister();
+    }
+
+    double StandardNormalRandomDeviate()
+    {
+        return mGenerateStandardNormal(mMersenneTwister);
     }
 };
